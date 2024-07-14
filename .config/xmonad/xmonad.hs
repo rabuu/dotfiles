fa36f60ccf8638c1e09323a445eb19532180b57b
Rasmus Buurman <dev@rbuurman.de>
2024-07-15 01:16:39 +0200

    Add xmonad.hs

---
+++ .config/xmonad/xmonad.hs
@@ -0,0 +1,17 @@
+import XMonad
+import XMonad.Config.Xfce
+import XMonad.Util.EZConfig (additionalKeysP)
+
+main = xmonad $ xfceConfig
+    { terminal    = "alacritty"
+    , modMask     = mod4Mask
+    , normalBorderColor = "#282828"
+    , focusedBorderColor = "#ebdbb2"
+    }
+    `additionalKeysP`
+    [ ("M-S-b", spawn "firefox")
+    , ("M-p", spawn "rofi -show drun")
+    , ("M-r", spawn "rofi -show run")
+    , ("M-C-p", spawn "rofi -show powermenu")
+    , ("M-S-p", spawn "passrofi")
+    ]
